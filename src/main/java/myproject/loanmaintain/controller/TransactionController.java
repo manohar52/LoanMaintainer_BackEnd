9e7b739392edd61e2f9d861367a5c2def9c8e97c
Revanth Pamulapati <revanth.pamulapati@ibm.com>
2021-04-20 22:12:03 -0400

    Added TransactionController with add new Transaction endpoint

--- src/main/java/myproject/loanmaintain/controller/TransactionController.java
+++ src/main/java/myproject/loanmaintain/controller/TransactionController.java
@@ -3,9 +3,13 @@ package myproject.loanmaintain.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -45,4 +49,19 @@ public class TransactionController {
 		int id = userRepo.getUserByUsername(receiver);
 		return transactionRepo.getTransactionsByReceiver(id);
 	}
+	@Autowired
+	private TransactionRepository transactionRepository;
+	
+	@ApiOperation(value="add new transaction")
+	@ApiResponses(value= {
+			@ApiResponse(code=200,message="Successfull"),
+			@ApiResponse(code=400,message="Bad Request"),
+			@ApiResponse(code=404,message="Not found"),
+			@ApiResponse(code=500,message="Internal Server error"),
+	})
+	@PostMapping("/addNew")
+	public ResponseEntity<Transaction> addTransaction(@RequestBody Transaction transaction) {
+		System.out.println("Inside add Transaction");
+		return ResponseEntity.ok(transactionRepository.save(transaction));
+	}	
 }
--- src/main/java/myproject/loanmaintain/repository/TransactionRepository.java
+++ src/main/java/myproject/loanmaintain/repository/TransactionRepository.java
@@ -5,14 +5,17 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import myproject.loanmaintain.model.Transaction;
 
-public interface TransactionRepository extends JpaRepository<Transaction, Integer> {
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 
 	@Query(value="select t from Transaction t where t.sender.userId = :sender")
 	public List<Transaction> getTransactionsBySender(@Param("sender") int sender);
 	
 	@Query(value="select t from Transaction t where t.receiver.userId = :receiver")
 	public List<Transaction> getTransactionsByReceiver(@Param("receiver") int receiver);
+	
 }
